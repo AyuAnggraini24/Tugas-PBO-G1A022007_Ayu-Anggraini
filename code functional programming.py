1b3545fc42391525ece91265a1e14e011fabfa50
AyuAnggraini24 <131652628+AyuAnggraini24@users.noreply.github.com>
2023-04-24 15:47:58 +0700

    Create code functional programming.py

---
+++ code functional programming.py
@@ -0,0 +1,10 @@
+def calculate_area(width, height):
+    return width * height
+
+def calculate_perimeter(width, height):
+    return 2 * (width + height)
+
+width = 4
+height = 5
+print(calculate_area(width, height)) 
+print(calculate_perimeter(width, height)) 
